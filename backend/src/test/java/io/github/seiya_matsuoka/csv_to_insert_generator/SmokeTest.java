eb351a1e9a80f271a23832056fb6fcf86b088908
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-01-08 16:16:51 +0900

    test: JUnitのSmokeTestを追加

---
+++ backend/src/test/java/io/github/seiya_matsuoka/csv_to_insert_generator/SmokeTest.java
@@ -0,0 +1,14 @@
+package io.github.seiya_matsuoka.csv_to_insert_generator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/** JUnit（JUnit 5）が正しく動作することを確認するためのスモークテスト。 */
+public class SmokeTest {
+
+  @Test
+  void smoke() {
+    assertTrue(true);
+  }
+}
