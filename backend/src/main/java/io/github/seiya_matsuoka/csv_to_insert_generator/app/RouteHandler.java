5eed5dec6e9e5ed3bc870d767fc8791849c0ddb1
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-01-28 20:38:52 +0900

    docs: RouteHandler のJavadocとコメントを追記

--- backend/src/main/java/io/github/seiya_matsuoka/csv_to_insert_generator/app/RouteHandler.java
+++ backend/src/main/java/io/github/seiya_matsuoka/csv_to_insert_generator/app/RouteHandler.java
@@ -2,7 +2,13 @@ package io.github.seiya_matsuoka.csv_to_insert_generator.app;
 
 import com.sun.net.httpserver.HttpExchange;
 
-/** Router から呼び出されるアプリ側ハンドラ。 */
+/**
+ * Router から呼び出されるアプリ側ハンドラ。
+ *
+ * <p>標準の HttpHandler は checked exception を投げられないため、 ハンドラ内で例外処理が煩雑になりやすい。
+ *
+ * <p>このインターフェースは throws Exception を許可し、 Router 側で「例外を共通的に 500 に落とす」などの処理を一括管理しやすくする。
+ */
 @FunctionalInterface
 public interface RouteHandler {
 
